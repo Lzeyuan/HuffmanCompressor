773d9ada8bf8188833ff69bfcbdf6cd836fba9f0
leza <1046527915@qq.com>
2026-02-24 10:55:52 +0800

    完成写入接口和对应单元测试。

--- test/BitWriterTest.cpp
+++ test/BitWriterTest.cpp
@@ -1,3 +1,4 @@
+#include <bit>
 #include <cstdint>
 #include <sstream>
 
@@ -96,18 +97,148 @@ TEST_F(BitWriterTest, VerifyBitOrderLogic) {
   EXPECT_EQ(static_cast<unsigned char>(getResult()[0]), 0b1000'0000);
 }
 
-TEST(IByteSinkTest, WriteFloat) {
-  using namespace leza::compression::huffman::util;
+class IByteSinkTest : public ::testing::Test {
+protected:
+  void SetUp() override {
+    ss_.str("");
+    ss_.clear();
+  }
+
+  // 辅助函数：获取当前写入的字符串内容
+  std::string getResult() { return ss_.str(); }
+
   std::stringstream ss_;
   leza::compression::huffman::util::OstreamSink sink_{ss_};
+};
 
-  float input = 0.26f;
-  sink_.writeInLittleEndian(0.26f);
+TEST_F(IByteSinkTest, WriteUInt16LittleEndian) {
+  using namespace leza::compression::huffman::util;
+
+  constexpr uint16_t expect = 10086;
+  sink_.writeInLittleEndian(expect);
+
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[0]), expect & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[1]), expect >> 8 & 0xff);
+}
+
+TEST_F(IByteSinkTest, WriteUInt32LittleEndian) {
+  using namespace leza::compression::huffman::util;
+
+  constexpr uint32_t expect = 10086;
+  sink_.writeInLittleEndian(expect);
+
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[0]), expect & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[1]), expect >> 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[2]), expect >> 2 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[3]), expect >> 3 * 8 & 0xff);
+}
+
+TEST_F(IByteSinkTest, WriteUInt64LittleEndian) {
+  using namespace leza::compression::huffman::util;
+
+  constexpr uint64_t expect = 10086;
+  sink_.writeInLittleEndian(expect);
+
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[0]), expect & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[1]), expect >> 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[2]), expect >> 2 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[3]), expect >> 3 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[4]), expect >> 4 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[5]), expect >> 5 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[6]), expect >> 6 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[7]), expect >> 7 * 8 & 0xff);
+}
+
+TEST_F(IByteSinkTest, WriteInt16LittleEndian) {
+  using namespace leza::compression::huffman::util;
+
+  constexpr int16_t expect = 0X3F3F;
+  sink_.writeInLittleEndian(expect);
+
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[0]), expect & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[1]), expect >> 8 & 0xff);
+}
+
+TEST_F(IByteSinkTest, WriteInt32LittleEndian) {
+  using namespace leza::compression::huffman::util;
+
+  constexpr int32_t expect = 0X3F3F3F3F;
+  sink_.writeInLittleEndian(expect);
+
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[0]), expect & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[1]), expect >> 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[2]), expect >> 2 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[3]), expect >> 3 * 8 & 0xff);
+}
+
+TEST_F(IByteSinkTest, WriteInt64LittleEndian) {
+  using namespace leza::compression::huffman::util;
+
+  constexpr int64_t expect = 0X3F3F3F3F;
+  sink_.writeInLittleEndian(expect);
+
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[0]), expect & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[1]), expect >> 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[2]), expect >> 2 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[3]), expect >> 3 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[4]), expect >> 4 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[5]), expect >> 5 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[6]), expect >> 6 * 8 & 0xff);
+  EXPECT_EQ(static_cast<uint8_t>(ss_.str()[7]), expect >> 7 * 8 & 0xff);
+}
+
+TEST_F(IByteSinkTest, WriteFloat) {
+  using namespace leza::compression::huffman::util;
+
+  constexpr float input = 0.26f;
+  sink_.writeInLittleEndian(input);
 
   using UintT = typename IByteSink::UintSelector<sizeof(input)>::type;
   UintT expect = std::bit_cast<UintT>(input);
-  ASSERT_EQ(static_cast<uint8_t>(ss_.str()[0]), expect & 0xff);
-  ASSERT_EQ(static_cast<uint8_t>(ss_.str()[1]), (expect) >> 8 & 0xff);
-  ASSERT_EQ(static_cast<uint8_t>(ss_.str()[2]), (expect) >> 2 * 8 & 0xff);
-  ASSERT_EQ(static_cast<uint8_t>(ss_.str()[3]), (expect) >> 3 * 8 & 0xff);
+  // 不是想省略代码，float之类的类型大小是未知的，看平台实现。
+  // 一般float是四字节
+  // ASSERT_EQ(static_cast<uint8_t>(ss_.str()[0]), expect & 0xff);
+  // ASSERT_EQ(static_cast<uint8_t>(ss_.str()[1]), expect >> 8 & 0xff);
+  // ASSERT_EQ(static_cast<uint8_t>(ss_.str()[2]), expect >> 2 * 8 & 0xff);
+  // ASSERT_EQ(static_cast<uint8_t>(ss_.str()[3]), expect >> 3 * 8 & 0xff);
+  for (int i = 0; i < sizeof(input); i++) {
+    EXPECT_EQ(static_cast<uint8_t>(ss_.str()[i]), (expect >> i * 8) & 0xff);
+  }
+
+  // 同理
+  // UintT restore = 0;
+  // restore |= static_cast<uint8_t>(ss_.str()[3]);
+  // restore <<= 8;
+  // restore |= static_cast<uint8_t>(ss_.str()[2]);
+  // restore <<= 8;
+  // restore |= static_cast<uint8_t>(ss_.str()[1]);
+  // restore <<= 8;
+  // restore |= static_cast<uint8_t>(ss_.str()[0]);
+  UintT restore = 0;
+  for (int i = sizeof(input) - 1; i >= 0; --i) {
+    restore <<= 8;
+    restore |= static_cast<uint8_t>(ss_.str()[i]);
+  }
+  ASSERT_EQ(std::bit_cast<float>(restore), input);
+}
+
+TEST_F(IByteSinkTest, WriteDouble) {
+  using namespace leza::compression::huffman::util;
+
+  constexpr double input = 0.10086;
+  sink_.writeInLittleEndian(input);
+
+  using UintT = typename IByteSink::UintSelector<sizeof(input)>::type;
+  UintT expect = std::bit_cast<UintT>(input);
+
+  for (int i = 0; i < sizeof(input); i++) {
+    EXPECT_EQ(static_cast<uint8_t>(ss_.str()[i]), (expect >> i * 8) & 0xff);
+  }
+
+  UintT restore = 0;
+  for (int i = sizeof(input) - 1; i >= 0; --i) {
+    restore <<= 8;
+    restore |= static_cast<uint8_t>(ss_.str()[i]);
+  }
+  ASSERT_EQ(std::bit_cast<double>(restore), input);
 }
